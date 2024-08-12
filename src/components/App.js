c5e8b484c73c6b32e2ee24381817b857f137f957
Marlon Munoz <marlon@marlonshade.com>
2024-08-12 06:43:16 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+
+    const [loaded, setLoaded] = useState(false);
+    const [imageUrl, setImageUrl] = useState("");
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then((data) => {
+            setImageUrl(data.message)
+            setLoaded(true)
+        }) 
+    }, []);
+
+    if (!loaded) return <p>Loading...</p>
+    return (
+        <div>
+            {<img src={imageUrl} alt="A Random Dog"/>}
+        </div>
+    )
+}
+
+export default App;
